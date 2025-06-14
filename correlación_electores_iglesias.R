d87b0601dc3c265cb5f32898e1a96e1bd6b9be3a
josesotoxi <jose.sotoc@unmsm.edu.pe>
2025-06-14 06:48:22 -0500

    Add files via upload

---
+++ correlación_electores_iglesias.R
@@ -0,0 +1,24 @@
+---
+  title: "R Notebook"
+output: html_notebook
+---
+  
+  ```{r}
+linkmerge='https://github.com/josesotoxi/practica/raw/refs/heads/main/final.csv'
+merge=read.csv(linkmerge)
+str(merge)
+```
+```{r}
+install.packages("car")
+
+bi2_1=formula(electores~iglesias)
+car::scatterplot(bi2_1, data = merge, regLine=list(col='red'),
+                 smooth = FALSE, grid = FALSE, frame = FALSE)
+```
+```{r}
+bi2_1Corr=formula(~ electores + iglesias)
+cor.test(bi2_1Corr,data=merge)[c('estimate','p.value')]
+```
+
+```{r}
+cor.test(bi2_1Corr,data=merge,method='spearman',exact=F)[c('estimate','p.value')]

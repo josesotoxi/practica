5da7c1ee2cd380f158b0db2b54f13c1dc3094592
josesotoxi <jose.sotoc@unmsm.edu.pe>
2025-06-14 04:11:38 -0500

    Add files via upload

---
+++ regresión_electores_iglesias.R
@@ -0,0 +1,22 @@
+---
+  title: "R Notebook"
+output:
+  pdf_document: default
+html_notebook: default
+---
+  
+  ```{r}
+linkMerge='https://github.com/josesotoxi/practica/raw/refs/heads/main/final.csv'
+merge=read.csv(linkMerge)
+str(merge)
+```
+Analisis de regresion en R para saber si la iglesia afecta la proporcion de votos de Aliaga, y de que manera si fuera el caso.
+
+```{r}
+h1=formula(electores~iglesias)
+
+r1=lm(h1,data=merge)
+
+summary(r1)
+
+```
